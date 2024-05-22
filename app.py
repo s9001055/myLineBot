af843e10e017e4a73723668c3780e8b2a121dab8
Arthur Chang <s9001055@gmail.com>
2024-05-22 22:49:34 +0800

    remove print

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print(event.reply_token)
+    # print(event.reply_token)
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
