ba25bd2b07bf89231eb38d6b5541530994758592
Arthur Chang <s9001055@gmail.com>
2024-06-26 22:12:22 +0800

    modify reply userid

--- app.py
+++ app.py
@@ -42,9 +42,10 @@ def handle_message(event):
         momName = data[event.message.text]
  
     message = TextSendMessage(text=momName)
-    print(event.source.userId)
-    print(type(event.source.userId))
-    line_bot_api.push_message('U5a24e475af75ef9f17e6c12877b10539', TextSendMessage(text=event.source.userId))
+    # print(event.source.userId)
+    # print(type(event.source.userId))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.source.user_id))
+    # line_bot_api.push_message('U5a24e475af75ef9f17e6c12877b10539', TextSendMessage(text=event.source.userId))
     # line_bot_api.reply_message(event.reply_token, message)
 
 if __name__ == "__main__":
