9384cefb0eda9a5adf8366c564a70cc95c6d387f
arthur <s9001055@gmail.com>
2024-11-01 23:30:35 +0800

    import requests

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+import requests
 import json
 import csv
 import os
