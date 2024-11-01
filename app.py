f741c80500b10d3b7bc375d058d240576f35ec65
arthur <s9001055@gmail.com>
2024-11-01 21:58:11 +0800

    reply latitude and longitude

--- app.py
+++ app.py
@@ -40,7 +40,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    latitude = event.message.latitude
+    longitude = event.message.longitude
+
+    replyMsg = str(latitude) + ", " + str(longitude)
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=replyMsg))
     # # 註冊家長
     # if '註冊家長' in event.message.text:
     #     arr = event.message.text.split('\n')
